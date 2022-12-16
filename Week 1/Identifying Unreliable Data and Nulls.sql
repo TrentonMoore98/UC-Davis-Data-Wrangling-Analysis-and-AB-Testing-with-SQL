ad491f611dec19caf02474af9e3b36aca41a0e34
TrentonMoore98 <70575986+TrentonMoore98@users.noreply.github.com>
2022-12-16 12:06:24 -0500

    Update Identifying Unreliable Data and Nulls.sql

--- Week 1/Identifying Unreliable Data and Nulls.sql
+++ Week 1/Identifying Unreliable Data and Nulls.sql
@@ -11,7 +11,7 @@ GROUP BY date;
 -- unless the scope of the project is focused on January 2017.
 
 
---Exercise 2:
+-- Exercise 2:
 -- Using any methods you like, determine if you can you trust this events table. (HINT: When did we start recording events on mobile)
 
 SELECT MIN(date(event_time)) as dater, MAX(platform)
@@ -20,6 +20,17 @@ GROUP BY platform;
 
 -- From analysis, capturing of Android and iOS data started in 2016, 
 --compared to mobile web 2013 and server and web data starting capture in 2012.
+
+-- Exercise 3:
+-- Count item views by category
+-- You found this table item_views_temp -- can you use it?
+-- hint: this table doesn't have dates.
+
+SELECT *
+FROM dsv1069.item_views_by_category_temp
+
+
+-- not a good idea to use this table, as it has temp in the name...
 
 
 
